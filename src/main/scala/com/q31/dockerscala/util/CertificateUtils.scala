6e853a9d634a945e34c57cbec6b57316a8aa6e91
joesan <codeintheopen@gmail.com>
2014-11-20 21:27:22 +0100

    further implementation

--- src/main/scala/com/q31/dockerscala/util/CertificateUtils.scala
+++ src/main/scala/com/q31/dockerscala/util/CertificateUtils.scala
@@ -1,6 +1,19 @@
 package com.q31.dockerscala.util
 
-import java.io.File
+import java.io.{FileReader, BufferedReader, File}
+
+import java.security._
+import java.security.cert.Certificate
+import java.security.cert.CertificateException
+import java.security.spec.InvalidKeySpecException
+import java.security.spec.PKCS8EncodedKeySpec
+import java.security.spec.X509EncodedKeySpec
+import org.bouncycastle.cert.X509CertificateHolder
+import org.bouncycastle.cert.jcajce.JcaX509CertificateConverter
+import org.bouncycastle.openssl.PEMKeyPair
+import org.bouncycastle.openssl.PEMParser
+import scala.util.Try
+;
 
 /**
  * @author Joe San (codeintheopen@gmail.com)
@@ -11,5 +24,79 @@ object CertificateUtils {
     List("ca.pem", "cert.pem", "key.pem").forall { case str => new File(dockerCertPath, str).exists }
   }
 
+  def createKeyStore(dockerCertPath: String) = Try {
+    val keyPair = loadPrivateKey(dockerCertPath)
+    val privateCertificate = loadCertificate(dockerCertPath)
+    val keyStore = KeyStore.getInstance("JKS")
+    keyStore.load(null)
+    keyStore.setKeyEntry("docker", keyPair.getPrivate(), "docker".toCharArray(), new Certificate[]{privateCertificate})
+    return keyStore
+  }
 
+  def createTrustStore(dockerCertPath: String) = {
+    val caPath = new File(dockerCertPath, "ca.pem");
+    val reader = new BufferedReader(new FileReader(caPath));
+    PEMParser pemParser = null;
+    try {
+      pemParser = new PEMParser(reader);
+      X509CertificateHolder certificateHolder = (X509CertificateHolder) pemParser.readObject();
+    Certificate caCertificate = new JcaX509CertificateConverter().setProvider("BC").getCertificate(certificateHolder);
+    KeyStore trustStore = KeyStore.getInstance("JKS");
+    trustStore.load(null);
+    trustStore.setCertificateEntry("ca", caCertificate);
+    return trustStore;
+    }
+    finally {
+      if(pemParser != null) {
+        IOUtils.closeQuietly(pemParser);
+      }
+      if(reader != null) {
+        IOUtils.closeQuietly(reader);
+      }
+    }
+  }
+
+  private def loadCertificate(dockerCertPath: String) = {
+    val certificate = new File(dockerCertPath, "cert.pem")
+    val reader = new BufferedReader(new FileReader(certificate))
+    PEMParser pemParser = null;
+    try {
+      pemParser = new PEMParser(reader);
+      X509CertificateHolder certificateHolder = (X509CertificateHolder) pemParser.readObject();
+    return new JcaX509CertificateConverter().setProvider("BC").getCertificate(certificateHolder);
+    }
+    finally {
+      if(pemParser != null) {
+        IOUtils.closeQuietly(pemParser);
+      }
+      if(reader != null) {
+        IOUtils.closeQuietly(reader);
+      }
+    }
+  }
+
+  private def tryReadCertificate(file: File): Try[BufferedReader] = Try { new BufferedReader(new FileReader(file)) }
+
+  private def tryLoadPemParser(reader: BufferedReader): Try[PEMParser] = Try { new PEMParser(reader) }
+
+  private def createKeyPair(buffReader: BufferedReader, pemParser: PEMParser) = {
+    val pemKeyPair = pemParser.readObject().asInstanceOf[PEMKeyPair]
+    val pemPrivateKeyEncoded = pemKeyPair.getPrivateKeyInfo().getEncoded()
+    val pemPublicKeyEncoded = pemKeyPair.getPublicKeyInfo().getEncoded()
+    val factory = KeyFactory.getInstance("RSA")
+    val publicKeySpec = new X509EncodedKeySpec(pemPublicKeyEncoded)
+    val privateKeySpec = new PKCS8EncodedKeySpec(pemPrivateKeyEncoded)
+    new KeyPair(factory.generatePublic(publicKeySpec), factory.generatePrivate(privateKeySpec))
+  }
+
+  private def loadPrivateKey(dockerCertPath: String) = Try {
+    val certReaderTry = tryReadCertificate(new File(dockerCertPath, "myKey.pem"))
+    for {
+      certReader <- certReaderTry
+      pemParser  <- tryLoadPemParser(certReader)
+    } yield createKeyPair(certReader, pemParser)
+
+    certReaderTry foreach(_.close)
+    pemParserTry foreach (_.close)
+  }
 }
